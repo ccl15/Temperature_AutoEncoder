4409c5534643ec0d613f8b577d1003adaaa3f7a1
ccl <kern159@gmail.com>
2023-07-19 09:52:59 +0800

    [fix] plot

--- output/hr_w_th_r_line.py
+++ output/hr_w_th_r_line.py
@@ -1,20 +1,39 @@
 """
 Singal station different hour and weight line plot.
 """
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 12})
 
+
+
+def MAE(yt, yp):
+    return np.mean(abs(yt-yp))
+
+def _load_staion_both(sid):
+    pred_path = f'{exp_dir}/AE_{sid}/{sid}_18t21_good.npy'
+    pred_g = np.load(pred_path)
+    pred_path = f'{exp_dir}/AE_{sid}/{sid}_18t21_bad.npy'
+    pred_b = np.load(pred_path)
+    
+    obs_path = f'../data/2ds_46/{sid}_H24_18t21.h5'
+    with h5py.File(obs_path, 'r') as f:
+        obs_g = f['good/temp'][:]
+        obs_b = f['bad/temp'][:]
+        time_b = f['bad/time'][:]
+    return pred_g, obs_g, pred_b, obs_b, time_b
 
 def recall_rate(good, bad, weight):
     mean = np.mean(good)
     std = np.std(good)
     th = mean + weight * std
-    # th = mean*weight
     r =  sum(bad <= th)/len(bad)
-    # print(weight, mean, std, th, r)
-    return th
+    return r
 
 
 def recall_plot():
-    weight = [0,0.1,0.2]
+    weight = [3, 2.5, 2]
     # weight = [1,1.5]
     MAE_good = [ [MAE(pred_g[i][-n:], obs_g[i][-n:]) for i in range(len(obs_g))]  for n in range(1,11)] 
     MAE_bad  = [ [MAE(pred_b[i][-n:], obs_b[i][-n:]) for i in range(len(obs_b))]  for n in range(1,11)] 
@@ -25,7 +44,15 @@ def recall_plot():
     
     plt.legend()
     # plt.legend(loc='center left', bbox_to_anchor=(1, 0.9))
+    plt.title(sid)
+    plt.ylabel('Recall rate')
+    plt.xlabel('last n hours to calculate error')
     plt.xticks(range(10), range(1,11) ,fontsize = 10)
-    plt.savefig('fig/threshold_std_24.png', bbox_inches='tight', dpi = 200)
+    plt.savefig('fig/recall_{sid}.png', bbox_inches='tight', dpi = 200)
     
-# recall_plot()
+
+
+exp_dir = 'AE_station_24'
+sid = '466920'
+pred_g, obs_g, pred_b, obs_b, time_b = _load_staion_both(sid)
+recall_plot()
